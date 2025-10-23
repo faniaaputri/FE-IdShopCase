e403cf442e05de5feffd446a845c1ce1c2cd684f
faniaaputri <fania.putri2548@student.unri.ac.id>
2025-10-23 14:43:15 +0700

    memperbaiki unit test address

--- src/__test__/app/AddressPage.spec.tsx
+++ src/__test__/app/AddressPage.spec.tsx
@@ -1,9 +1,14 @@
-import AddressPage from "@/app/page";
+import AddressPage from "@/app/(customer)/account/address/page";
+import AboutLayout from "@/app/(customer)/account/layout";
 import { render } from "@testing-library/react";
 
 describe("Home", () => {
   it("should render", () => {
-    const page = render(<AddressPage />);
+    const page = render(
+      <AboutLayout>
+        <AddressPage />
+      </AboutLayout>
+    );
     expect(page).toMatchSnapshot();
   });
 });
